5d7e8053823db3e70655328e06b1fd43347d423d
innocent2102 <michal2102@gmail.com>
2021-03-14 20:51:11 +0100

    feat: add native camera functionality

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -11,21 +11,37 @@
           <ion-title size="large">Camera</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-button type="submit" @click="openCamera">Take a photo</ion-button>
+      <ion-button type="submit" @click="takePicture()">Take a photo</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { CameraResultType, CameraSource, Plugins } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 export default  {
   name: 'Camera',
   components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
 
   methods: {
-    openCamera () {
-      console.log('asda');
+    async takePicture() {
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing: true,
+        saveToGallery: true,
+        source: CameraSource.Camera,
+        resultType: CameraResultType.Uri
+      });
+      // image.webPath will contain a path that can be set as an image src.
+      // You can access the original file using image.path, which can be
+      // passed to the Filesystem API to read the raw data of the image,
+      // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+      const imageUrl = image.webPath;
+      // Can be set to the src of an image now
+      console.log(imageUrl);
     }
   }
 }
