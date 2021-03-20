4acfb44a9b07dd6963ba3156cf570bda9c1293fa
innocent2102 <michal2102@gmail.com>
2021-03-20 16:16:32 +0100

    feat: add root store

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 
 import { IonicVue } from '@ionic/vue';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import store from './store';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -26,7 +27,8 @@ import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(store);
 
 router.isReady().then(() => {
   app.mount('#app');
